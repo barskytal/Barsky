19e5d77789580fdd2caac374a5f8cf837483f4e1
barskytal <barskytal@gmail.com>
2023-09-15 12:37:22 -0700

    Project 1

---
+++ Project_1_Sept_8_23/src/MyClass.java
@@ -0,0 +1,46 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class MyClass {
+    public static void main(String[] args) {
+        int x=113;
+        double y=2.71828;
+        String z="Computer Science";
+        System.out.println("This is room " + x);
+        System.out.println("e is close to " + y);
+        System.out.println("I am learning a bit about " + z);
+        /*The Classes*/
+        String a= "Active Living";
+        String b= "Physics";
+        String c= "Computer Programming";
+        String d= "Pre-Calculus";
+        String e= "Free";
+        String f= "Chemistry";
+        String g= "English";
+        String h= "Calculus";
+
+        /*The Teachers*/
+        String i= "Mr. Hodgeson";
+        String j= "Mr. Tang";
+        String k= "Mr. Zaremba";
+        String l= "Ms. Reed";
+        String m= "No Teacher";
+        String n= "Mr. Slapsys";
+        String o= "Ms. Au";
+        String p= "Ms. Reed";
+
+        /*Schedule*/
+        System.out.println( "+-----------------------------------+" );
+        System.out.println( "|1|       "+a+"|"+i+"|");
+        System.out.println( "|2|             "+b+"|" +j+"    |");
+        System.out.println( "|3|"+c+"|"+k+" |");
+        System.out.println( "|4|        "+d+"|"+l+"    |");
+        System.out.println( "|5|                "+e+"|"+m+"  | ");
+        System.out.println( "|6|           "+f+"|" +n+" |");
+        System.out.println( "|7|             "+g+"|"+o+"      |");
+        System.out.println( "|8|            "+h+"|"+p+"    |" );
+        System.out.println( "+-----------------------------------+" );
+
+
+    }
+
+}
